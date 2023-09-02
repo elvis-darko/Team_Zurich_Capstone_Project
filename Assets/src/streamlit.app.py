456df69bc88a6c44cab6f03c3496b62599879b75
asus <princesstoy07@yahoo.com>
2023-09-02 11:52:06 +0100

    added images to the predict app

--- Assets/src/streamlit.app.py
+++ Assets/src/streamlit.app.py
@@ -6,63 +6,33 @@ import numpy as np
 model_path = r'D:\Projects\Team_Zurich_Capstone_Project\Assets\src\tuned_gb_model.joblib'
 tuned_gb_model = joblib.load(model_path)
 
-# Create a function for the About page content
-def about_page():
-    st.title('About the App')
-    st.write("Welcome to the Team Zurich Churn Prediction App!")
-    st.write("This app allows you to predict whether a customer will churn or not based on the provided features.")
-    st.write("Variable Definitions:")
-    
-# Streamlit UI
+  # Add the image using st.image
+image_url = "https://i.ytimg.com/vi/ocMd2loRfWE/maxresdefault.jpg"
+st.image(image_url, caption='Team Zurich Churn Prediction App', use_column_width=True)
+
+# Title of the app
 st.title('Team Zurich Churn Prediction App')
 
-# Create a menu to navigate between pages
-menu = ['Variable Definitions', 'Predict']
-choice = st.sidebar.selectbox('Select Page', menu)
-
-# Display the selected page
-if choice == 'Variable Definitions':
-    st.title('Variable Definitions')
-    st.write("Variable Definitions:")
-    st.write("**user_id**: Unique identifier for each client.")
-    st.write("**REGION**: Location of each client.")
-    st.write("**TENURE**: Duration in the network.")
-    st.write("**MONTANT**: Top-up amount.")
-    st.write("**FREQUENCE_RECH**: Number of times the customer refilled.")
-    st.write("**REVENUE**: Monthly income of each client.")
-    st.write("**ARPU_SEGMENT**: Income over 90 days / 3.")
-    st.write("**FREQUENCE**: Number of times the client has made an income.")
-    st.write("**DATA_VOLUME**: Number of connections.")
-    st.write("**ON_NET**: Inter-expresso call.")
-    st.write("**ORANGE**: Call to Orange.")
-    st.write("**TIGO**: Call to Tigo.")
-    st.write("**ZONE1**: Call to Zone1.")
-    st.write("**ZONE2**: Call to Zone2.")
-    st.write("**MRG**: A client who is going.")
-    st.write("**REGULARITY**: Number of times the client is active for 90 days.")
-    st.write("**TOP_PACK**: The most active packs.")
-    st.write("**FREQ_TOP_PACK**: Number of times the client has activated the top pack packages.")
-    st.write("**CHURN**: Variable to predict - Target.")
-    
-else:
-    # Input form and prediction logic
-    st.title('Predict Churn')
-
+# HOW TO USE THE APP
+st.sidebar.title("How to Use the App")
+st.sidebar.write("1. Provide the necessary input features.")
+st.sidebar.write("2. Click the 'Predict")
+                 
 # Input form
-tenure = st.slider('Tenure', 1, 12, 7)
-montant = st.number_input('Montant', value=0.0)
-frequence_rech = st.number_input('Frequence Recharge', value=0.0)
-revenue = st.number_input('Revenue', value=0.0)
-arpu_segment = st.number_input('ARPU Segment', value=0.0)
-frequence = st.number_input('Frequence', value=0.0)
-data_volume = st.number_input('Data Volume', value=0.0)
-on_net = st.number_input('On Net', value=0.0)
-orange = st.number_input('Orange', value=0.0)
-tigo = st.number_input('Tigo', value=0.0)
-zone1 = st.number_input('Zone1', value=0.0)
-zone2 = st.number_input('Zone2', value=0.0)
-regularity = st.slider('Regularity', 1, 61, 30)
-freq_top_pack = st.number_input('Frequency Top Pack', value=0.0)
+tenure = st.slider('Tenure: Duration in the network', 1, 12, 7)
+montant = st.number_input('Montant:Top-up amount', value=0.0)
+frequence_rech = st.number_input('Frequence Recharge: Number of times the customer refilled', value=0.0)
+revenue = st.number_input('Revenue: Monthly income of each client', value=0.0)
+arpu_segment = st.number_input('ARPU Segment:  Income over 90 days / 3', value=0.0)
+frequence = st.number_input('Frequency: Number of times the customer refilled', value=0.0)
+data_volume = st.number_input('Data Volume:  Number of connections', value=0.0)
+on_net = st.number_input('On Net:Inter-expresso call', value=0.0)
+orange = st.number_input('Orange :Call to Orange', value=0.0)
+tigo = st.number_input('Tigo:  Call to Tigo', value=0.0)
+zone1 = st.number_input('Zone1: Call to Zone1', value=0.0)
+zone2 = st.number_input('Zone2: Call to Zone2', value=0.0)
+regularity = st.slider('Regularity: Number of times the client is active for 90 days', 1, 61, 30)
+freq_top_pack = st.number_input('Frequency Top Pack:Number of times the client has activated the top pack packages', value=0.0)
 total_recharge = st.number_input('Total Recharge', value=0.0)
 avg_revenue_montant = st.number_input('Average Revenue Montant', value=0.0)
 frequence_squared = st.number_input('Frequence Squared', value=0.0)
