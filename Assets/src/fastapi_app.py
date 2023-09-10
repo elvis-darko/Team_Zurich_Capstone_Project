ee7b4e7e5ec4879a28262935f65cc41b74494108
asus <princesstoy07@yahoo.com>
2023-09-10 17:03:49 +0100

    upda

--- Assets/src/fastapi_app.py
+++ Assets/src/fastapi_app.py
@@ -1,18 +1,15 @@
-import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
-import matplotlib.pyplot as plt
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 
-# Load the saved tuned Gradient Boosting model
-model_path = r'D:\Projects\Team_Zurich_Capstone_Project\Assets\src\tuned_gb_model.joblib'
+# Load the model here
+model_path = r"path_to_your_model_file.joblib"
 tuned_gb_model = joblib.load(model_path)
 
-# Create a FastAPI app
 app = FastAPI()
 
-class InputFeatures(BaseModel):
+class PredictionRequest(BaseModel):
     tenure: int
     montant: float
     frequence_rech: float
@@ -22,7 +19,7 @@ class InputFeatures(BaseModel):
     data_volume: float
     on_net: float
     orange: float
-    tigo: float
+    tigo2: float
     zone1: float
     zone2: float
     regularity: int
@@ -32,31 +29,14 @@ class InputFeatures(BaseModel):
     frequence_squared: float
     on_net_reg_ratio: float
 
-# Streamlit prediction function
-def predict_churn(input_data):
-    input_features = np.array([list(input_data.dict().values())])
-    prediction = tuned_gb_model.predict(input_features)
-    prediction_probability = tuned_gb_model.predict_proba(input_features)
-
-    result = {
-        'prediction': int(prediction[0]),
-        'churn_probability': float(prediction_probability[0][1])
-    }
-    return result
-
-# FastAPI endpoint to predict churn
 @app.post("/predict/")
-async def predict(input_data: InputFeatures):
-    try:
-        result = predict_churn(input_data)
-        return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-# FastAPI endpoint to serve the Streamlit app
-@app.get("/streamlit/")
-async def get_streamlit_app():
-    st_app_html = """
-    <iframe src="http://localhost:8501" width="1000" height="800"></iframe>
-    """
-    return st_app_html
+async def predict_churn(request: PredictionRequest):
+    input_features = np.array([list(request.dict().values())])
+    prediction = tuned_gb_model.predict(input_features)
+    prediction_probability = tuned_gb_model.predict_proba(input_features)[:, 1]
+    
+    response = {
+        "prediction": prediction[0],
+        "churn_probability": round(prediction_probability[0] * 100, 2),
+    }
+    return response
